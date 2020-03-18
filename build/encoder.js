ba9efe4c2760bfccc3d19b92c09d0e79533e4898
scintilla4evr <kacper17@outlook.com>
2020-03-18 18:35:30 +0100

    Removed RLE compression, it was causing issues

--- build/encoder.js
+++ build/encoder.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function encodeOctets(life) {
     const size = Math.ceil(life.width * life.height / 8);
-    const out = new Int8Array(size);
+    const out = new Uint8Array(size);
     let pos = 0, current = 0, counter = 0;
     for (let y = 0; y < life.height; y++) {
         for (let x = 0; x < life.width; x++) {
@@ -22,33 +22,18 @@ function encodeOctets(life) {
     return out;
 }
 exports.encodeOctets = encodeOctets;
-function rleCompress(octets) {
-    const out = [];
-    let chainLength = 0, chainBody = 0;
+function dummyRLE(octets) {
+    const out = Buffer.alloc(octets.length * 2);
     for (let i = 0; i < octets.length; i++) {
-        if (chainLength && chainBody !== octets[i]) {
-            out.push(chainLength);
-            out.push(chainBody);
-            chainLength = 0;
-        }
-        if (!chainLength) {
-            chainLength = 1;
-            chainBody = octets[i];
-        }
-        else if (chainLength && chainBody === octets[i]) {
-            chainLength++;
-        }
+        out.writeUInt8(1, 2 * i);
+        out.writeUInt8(octets[i], 2 * i + 1);
     }
-    if (chainLength) {
-        out.push(chainLength);
-        out.push(chainBody);
-    }
-    return new Int8Array(out);
+    return out;
 }
-exports.rleCompress = rleCompress;
+exports.dummyRLE = dummyRLE;
 function encodeLife(life) {
     const header = Buffer.alloc(12);
-    const rle = Buffer.from(rleCompress(encodeOctets(life)));
+    const rle = dummyRLE(encodeOctets(life));
     header.write("LIFE");
     header.writeUInt32LE(life.width, 4);
     header.writeUInt32LE(life.height, 8);
--- src/encoder.ts
+++ src/encoder.ts
@@ -1,8 +1,8 @@
 import { LifeData } from "./decoder";
 
-export function encodeOctets(life: LifeData): Int8Array {
+export function encodeOctets(life: LifeData): Uint8Array {
     const size = Math.ceil(life.width * life.height / 8)
-    const out = new Int8Array(size)
+    const out = new Uint8Array(size)
 
     let pos = 0, current = 0, counter = 0
 
@@ -28,39 +28,20 @@ export function encodeOctets(life: LifeData): Int8Array {
     return out
 }
 
-export function rleCompress(octets: Int8Array): Int8Array {
-    const out: number[] = []
-    let chainLength = 0, chainBody = 0
+export function dummyRLE(octets: Uint8Array): Buffer {
+    const out = Buffer.alloc(octets.length * 2)
 
     for (let i = 0; i < octets.length; i++) {
-        if (chainLength && chainBody !== octets[i]) {
-            out.push(chainLength)
-            out.push(chainBody)
-
-            chainLength = 0
-        }
-
-        if (!chainLength) {
-            chainLength = 1
-            chainBody = octets[i]
-        } else if (chainLength && chainBody === octets[i]) {
-            chainLength++
-        }
+        out.writeUInt8(1, 2 * i)
+        out.writeUInt8(octets[i], 2 * i + 1)
     }
 
-    if (chainLength) {
-        out.push(chainLength)
-        out.push(chainBody)
-    }
-
-    return new Int8Array(out)
+    return out
 }
 
 export function encodeLife(life: LifeData): Buffer {
     const header = Buffer.alloc(12)
-    const rle = Buffer.from(
-        rleCompress(encodeOctets(life))
-    )
+    const rle = dummyRLE(encodeOctets(life))
 
     header.write("LIFE")
     header.writeUInt32LE(life.width, 4)
